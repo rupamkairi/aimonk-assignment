f231f4068647690d4b61884c220119a80cab1f73
Rupam Kairi <rupamkairi@hotmail.com>
2024-10-27 11:14:27 +0530

    deploying with hosted python backend

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,2 +1,3 @@
-export const baseURL = ""; // next backend
+// export const baseURL = ""; // next backend
 // export const baseURL = "http://localhost:8000" // fastapi backend
+export const baseURL = "https://aimonk-assignment.onrender.com"; // fastapi backend
